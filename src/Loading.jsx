eeeaff9e2d6f7d99578ac4eb75274c3c923eab41
unknown <yashslg004@gmail.com>
2025-01-17 20:04:59 +0530

    Added Loading Component file

---
+++ src/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading()
+{
+    return(
+        <>
+            <h1 className="text-white text-4xl text-center rounded-lg bg-gray-600 p-4 mt-6 font-serif w-96 place-self-center">LOADING...</h1>
+        </>
+    )
+}
+export default Loading
