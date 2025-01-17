c8c812ce1025e8223151b110bea9860ba75d5b77
unknown <yashslg004@gmail.com>
2025-01-18 00:07:06 +0530

    Added Error Component file

---
+++ src/Error.jsx
@@ -0,0 +1,11 @@
+function Error()
+{
+    console.log("Error");
+    
+    return(
+        <h1 className="text-white text-4xl text-center rounded-lg bg-gray-600 p-4 mt-6 font-serif">
+            <span>🎇</span>There was an error in fetching questions
+        </h1>
+    )
+}
+export default Error
